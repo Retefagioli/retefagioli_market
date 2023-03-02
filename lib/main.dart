afd3f4ddf21089f0e64087c2f1777ded1e8b84b7
Blessium <alessandro.melgar04@gmail.com>
2023-03-02 17:41:24 +0100

    All SplashScreen stuff

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_native_splash/flutter_native_splash.dart';
 import 'config/app_router.dart';
 import 'package:firebase_core/firebase_core.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
+    FlutterNativeSplash.preserve(widgetsBinding: WidgetsBinding.instance);
   runApp(const MyApp());
 }
 
