5f8566104df8abddc8a8b9aecb09e5b28dbc636b
Blessium <alessandro.melgar04@gmail.com>
2023-03-03 15:58:12 +0100

    Added default transition fade

--- lib/config/app_router.dart
+++ lib/config/app_router.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:retefagioli_market/screens/screens.dart';
 import 'package:go_router/go_router.dart';
+import 'package:retefagioli_market/widgets/custom_transition_page.dart';
 
 final GoRouter router = GoRouter(
   routes: <GoRoute>[
@@ -13,23 +14,33 @@ final GoRouter router = GoRouter(
     GoRoute(
       name: "login",
       path: "/login",
-      builder: (BuildContext contex, GoRouterState state) {
-        return const LoginScreen();
-      },
+      builder: (BuildContext contex, GoRouterState state) =>
+          const LoginScreen(),
+      pageBuilder: (context, state) => buildPageWithDefaultTransition(
+        context: context,
+        state: state,
+        child: const LoginScreen(),
+      ),
     ),
     GoRoute(
       name: "home",
       path: '/home',
-      builder: (BuildContext context, GoRouterState state) {
-        return HomeScreen();
-      },
+      builder: (BuildContext context, GoRouterState state) => HomeScreen(),
+      pageBuilder: (context, state) => buildPageWithDefaultTransition(
+        context: context,
+        state: state,
+        child: HomeScreen(),
+      ),
     ),
     GoRoute(
       name: "register",
       path: "/register",
-      builder: (BuildContext context, GoRouterState state) {
-        return RegisterScreen();
-      },
+      builder: (BuildContext context, GoRouterState state) =>  RegisterScreen(),
+      pageBuilder: (context, state) => buildPageWithDefaultTransition(
+        context: context,
+        state: state,
+        child: RegisterScreen(),
+      ),
     ),
   ],
 );
