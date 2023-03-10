63cf2b6dfdeef9869c8dec3dae93e3723df0b1c2
Blessium <alessandro.melgar04@gmail.com>
2023-03-10 19:03:42 +0100

    Improved user model

--- lib/models/user_model.dart
+++ lib/models/user_model.dart
@@ -1,18 +1,11 @@
 class UserModel {
-  final String id;
-  final String name;
-  final String surname;
-  final DateTime? dob;
-  final String email;
-  final String? phone;
-  final String? address;
-  UserModel({
-    required this.id,
-    required this.name,
-    required this.surname,
-    this.dob,
-    required this.email,
-    this.phone,
-    this.address,
-  });
+  String? id;
+  String? name;
+  String? surname;
+  DateTime? dob;
+  String? email;
+  String? password;
+  String? phone;
+  String? address;
+  UserModel();
 }
