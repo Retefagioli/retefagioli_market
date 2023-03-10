b0a770a3b0bbe79cc1111351c42eaca48621a60f
Blessium <alessandro.melgar04@gmail.com>
2023-03-10 16:16:12 +0100

    Simple change

--- lib/widgets/widget_custom.dart
+++ lib/widgets/widget_custom.dart
@@ -2,4 +2,4 @@ export 'form_field_custom.dart';
 export 'custom_title.dart';
 export 'custom_login.dart';
 export 'custom_button.dart';
-export 'custom_registration.dart';
+export 'custom_registration_credentials.dart';
