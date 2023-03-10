3808be1ad36530f2ebb09d36ffa10f48b76ce8f8
Blessium <alessandro.melgar04@gmail.com>
2023-03-10 19:05:00 +0100

    Created personal info registration phase

--- lib/widgets/custom_registration_credentials.dart
+++ lib/widgets/custom_registration_credentials.dart
@@ -1,17 +1,22 @@
 import 'package:flutter/material.dart';
 import 'widget_custom.dart';
 import 'package:retefagioli_market/services/utils/validators.dart';
+import 'package:retefagioli_market/models/user_model.dart';
 
-class RegistrationFormCustom extends StatefulWidget {
+class RegistrationCredentialsForm extends StatefulWidget {
   final void Function() changeState;
-  const RegistrationFormCustom({Key? key, required this.changeState}) : super(key: key);
+  final UserModel userModel;
+  const RegistrationCredentialsForm(
+      {Key? key, required this.changeState, required this.userModel})
+      : super(key: key);
 
   @override
-  State<RegistrationFormCustom> createState() => _RegistrationFormCustomState();
+  State<RegistrationCredentialsForm> createState() =>
+      _RegistrationCredentialsFormState();
 }
 
-class _RegistrationFormCustomState extends State<RegistrationFormCustom> {
-  final TextEditingController usernameController = TextEditingController();
+class _RegistrationCredentialsFormState
+    extends State<RegistrationCredentialsForm> {
   final TextEditingController emailController = TextEditingController();
   final TextEditingController passwordController = TextEditingController();
   final TextEditingController secondPasswordController =
@@ -27,14 +32,6 @@ class _RegistrationFormCustomState extends State<RegistrationFormCustom> {
           Container(
             margin: const EdgeInsets.all(14.0),
             child: const TitleCustom(customText: "Registrati"),
-          ),
-          Container(
-            padding: const EdgeInsets.all(10.0),
-            child: FormFieldCustom(
-              customText: "Username",
-              validator: checkUsername,
-              controller: usernameController,
-            ),
           ),
           Container(
             padding: const EdgeInsets.all(10.0),
@@ -67,6 +64,8 @@ class _RegistrationFormCustomState extends State<RegistrationFormCustom> {
             child: ButtonCustom(
               customText: "Continua",
               onPressed: () => {
+                widget.userModel.email = emailController.text,
+                widget.userModel.password = passwordController.text,
                 widget.changeState()
               },
             ),
@@ -77,7 +76,8 @@ class _RegistrationFormCustomState extends State<RegistrationFormCustom> {
   }
 
   String? checkSecondPassword(String? secondPassword) {
-    if (passwordController.text != secondPassword) return "Le password non coincidono";
+    if (passwordController.text != secondPassword)
+      return "Le password non coincidono";
     return null;
   }
 }
