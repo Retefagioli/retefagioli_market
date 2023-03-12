b07a8414d7583511642065382f12a78effb5bbe1
Blessium <alessandro.melgar04@gmail.com>
2023-03-12 14:16:51 +0100

    Updated router for shell routing

--- lib/config/app_router.dart
+++ lib/config/app_router.dart
@@ -3,13 +3,38 @@ import 'package:retefagioli_market/screens/screens.dart';
 import 'package:go_router/go_router.dart';
 import 'package:retefagioli_market/widgets/custom_transition_page.dart';
 
+final shellRoutesNames = <String>[
+  "/",
+  "/search",
+  "/boh",
+  "/profile",
+];
+
+final _rootNavigationKey = GlobalKey<NavigatorState>();
+final _shellNavigationKey = GlobalKey<NavigatorState>();
 final GoRouter router = GoRouter(
-  routes: <GoRoute>[
-    GoRoute(
-      path: "/",
-      builder: (BuildContext context, GoRouterState state) {
-        return const RootScreen();
+  initialLocation: "/",
+  navigatorKey: _rootNavigationKey,
+  routes: [
+    ShellRoute(
+      navigatorKey: _shellNavigationKey,
+      builder: (context, state, child) {
+        return RootScreen(child: child);
       },
+      routes: [
+        GoRoute(
+          path: "/",
+          pageBuilder: (context, state) => NoTransitionPage(
+            child: HomeScreen(),
+          ),
+        ),
+        GoRoute(
+          path: "/profile",
+          pageBuilder: (context, state) => NoTransitionPage(
+            child: ProfileScreen(),
+          ),
+        ),
+      ],
     ),
     GoRoute(
       name: "login",
@@ -25,7 +50,8 @@ final GoRouter router = GoRouter(
     GoRoute(
       name: "register",
       path: "/register",
-      builder: (BuildContext context, GoRouterState state) =>  const RegisterScreen(),
+      builder: (BuildContext context, GoRouterState state) =>
+          const RegisterScreen(),
       pageBuilder: (context, state) => buildPageWithDefaultTransition(
         context: context,
         state: state,
