392f0cc1dc819dccff3d9ab85f3751fa5762f1b6
Blessium <alessandro.melgar04@gmail.com>
2023-03-12 13:21:38 +0100

    Added animated bottom navigation bar

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_native_splash/flutter_native_splash.dart';
-import 'package:retefagioli_market/services/authentication/userAuthentication.dart';
-
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:animated_bottom_navigation_bar/animated_bottom_navigation_bar.dart';
 import '../config/app_router.dart';
 
 class HomeScreen extends StatefulWidget {
@@ -12,7 +12,13 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
-  Authenticator authenticator = Authenticator();
+  final elementBar = <IconData>[
+    Icons.home,
+    Icons.search,
+    Icons.heart_broken,
+    Icons.account_box
+  ];
+  var _bottomNavIndex = 0;
   @override
   void initState() {
     super.initState();
@@ -27,14 +33,20 @@ class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: ElevatedButton(
-          onPressed: () async {
-            await authenticator.logoutUser();
-            router.go("/");
-          },
-          child: const Text("Clicca per uscire"),
-        ),
+      body: const Center(
+        child: Text("Home Page"),
+      ),
+      floatingActionButton: FloatingActionButton(
+          child: const Icon(Icons.shopping_cart), onPressed: () {}),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+      bottomNavigationBar: AnimatedBottomNavigationBar(
+        icons: elementBar,
+        gapLocation: GapLocation.center,
+        notchSmoothness: NotchSmoothness.defaultEdge,
+        activeIndex: _bottomNavIndex,
+        onTap: (index) => setState(() {
+          _bottomNavIndex = index;
+        }),
       ),
     );
   }
